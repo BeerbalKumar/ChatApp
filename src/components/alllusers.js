c58f29290ae442dd14aaf9ae5fa75787696f0519
BeerbalKumar <iambeerbalkumar@gmail.com>
2019-11-07 21:55:36 +0500

    chatapp

---
+++ src/components/alllusers.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {Image} from 'react-native'
+import { Container, Header, Content, Card, CardItem, Body, Text } from 'native-base';
+export default class CardExample extends Component {
+  render() {
+    return (
+
+            <CardItem >
+              <Body style={{display:"flex",flexDirection:"row"}}>
+              <Image style={{width: 50, height: 50,borderRadius:50}}source={{uri: this.props.userImage}}/>
+                <Text style={{color:"black",marginLeft:20,marginTop:15}}>
+
+                {
+                  
+                  this.props.userName
+
+                }
+                </Text>
+              </Body>
+            </CardItem>
+    
+    );
+  }
+}
---
+++ src/components/chat.js
@@ -0,0 +1,69 @@
+
+import React from 'react'
+import {View,Text,Image} from 'react-native'
+import firebase from './../Config/firebase'
+import Allusers from './alllusers'
+
+export default class Chat extends React.Component{
+
+    constructor(){
+        super();
+        this.state={
+          users:[],
+        }
+    }
+
+
+    componentDidMount(){
+        let {users} = this.state
+
+        firebase.auth().onAuthStateChanged((user) =>{
+            if (user) {
+             
+
+                firebase.firestore().collection("allUsers").get()
+                .then((data)=>{
+                    data.forEach((getData)=>{
+                        let usersData = getData.data();
+                 if(usersData.userId !== user.uid){
+                        users.push(usersData);
+                            this.setState({
+                                users:users
+                            })
+                        }
+                    })
+                })
+                .catch((error)=>{
+                    console.log("====================================================>error",error)
+                })
+
+            } else {
+                // this.props.navigation.navigate("FacebookLogin")
+                console.log("logged out")
+
+            }
+          });
+
+
+    }
+
+  
+  
+    render(){
+    
+        return(
+<View>
+   {
+       this.state.users.map((val,i)=>{
+
+           return(
+       <Allusers userName={val.userName} userImage={val.userImage}/>
+           )
+       })
+
+}
+
+</View>
+        )
+    }
+}
